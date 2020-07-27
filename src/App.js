351619284d19c123b88eb0b5d9dd1ec52a874eab
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-07-27 16:07:32 -0300

    add api and list all products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Main from './pages/main';
 
 import './styles.css';
 
@@ -7,6 +8,7 @@ const App = () => (
 
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
